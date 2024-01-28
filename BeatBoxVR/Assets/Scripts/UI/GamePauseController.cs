74e0160a0f478ccc9189fde58d8b990bff2b8bed
BattlestationX2\JDavies <MilesJMaiden@Gmail.com>
2024-01-28 21:15:47 +0000

    UIManager GO (No prefab yet ..see scene)

--- BeatBoxVR/Assets/Scripts/UI/GamePauseController.cs
+++ BeatBoxVR/Assets/Scripts/UI/GamePauseController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePauseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject countdownUI; // Reference to countdown UI element
+
+    public void PauseGame()
     {
-        
+        // Pause game logic
+        Time.timeScale = 0;
+        // Show pause menu...
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnpauseGameWithCountdown()
     {
-        
+        StartCoroutine(CountdownAndUnpause());
+    }
+
+    private IEnumerator CountdownAndUnpause()
+    {
+        // Show countdown UI and wait
+        countdownUI.SetActive(true);
+        // Countdown logic...
+        yield return new WaitForSecondsRealtime(3); // 3-second countdown
+
+        countdownUI.SetActive(false);
+        Time.timeScale = 1; // Resume game
     }
 }
