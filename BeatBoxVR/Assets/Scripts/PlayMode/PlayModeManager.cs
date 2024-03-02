fa4546ec4681b379b802443497987410b5a56c8e
BattlestationX2\JDavies <MilesJMaiden@Gmail.com>
2024-03-02 21:50:26 +0000

    PlayModeManager.CS - WIP (Every possible note combination), Now I need to create signal for each then assign to reciever.

--- BeatBoxVR/Assets/Scripts/PlayMode/PlayModeManager.cs
+++ BeatBoxVR/Assets/Scripts/PlayMode/PlayModeManager.cs
@@ -10,6 +10,18 @@ public class PlayModeManager : MonoBehaviour
     public float delayBeforeStart = 3.0f;     // Delay in seconds before a song starts
     public GameObject[] notePrefabs;          // Assign note prefabs in the Inspector
 
+    [Header("Note Prefabs")]
+    public GameObject hiHatPrefab;
+    public GameObject crashPrefab;
+    public GameObject snarePrefab;
+    // Add other prefabs as needed
+
+    [Header("Spawn Points")]
+    public Transform hiHatSpawnPoint;
+    public Transform crashSpawnPoint;
+    public Transform snareSpawnPoint;
+    // Add other spawn points as needed
+
     // Start is called before the first frame update
     void Start()
     {
@@ -77,7 +89,9 @@ public class PlayModeManager : MonoBehaviour
             Instantiate(notePrefabs[noteType], new Vector3(0, 0, 0), Quaternion.identity);
         }
     }
+    #region Notes
 
+    #region Individual Notes
     //Individual Notes
     public void SpawnHiHatNote() { SpawnNoteOfType(0); }
     public void SpawnCrashNote() { SpawnNoteOfType(1); }
@@ -88,7 +102,9 @@ public class PlayModeManager : MonoBehaviour
     public void SpawnFloorTomNote() { SpawnNoteOfType(6); }
     public void SpawnSplashNote() { SpawnNoteOfType(7); }
     public void SpawnRideNote() { SpawnNoteOfType(8); }
+    #endregion
 
+    #region DoubleNotes
     //DoubleNotes
     public void SpawnHiHatCrashNotes() { 
         
@@ -103,10 +119,685 @@ public class PlayModeManager : MonoBehaviour
         SpawnNoteOfType(2);
 
     }
+    public void SpawnHiHatSmallTomhNotes()
+    {
 
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnHiHatKickDrumhNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnHiHatMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatRidehNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnCrashSnareNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(2);
+
+    }
+    public void SpawnCrashSmallTomNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnCrashKickDrumNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnCrashMediumTomNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnCrashFloorTomNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnCrashSplashNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnCrashRideNotes()
+    {
+
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnSnareSmallTomNotes()
+    {
+
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnSnareKickDrumTomNotes()
+    {
+
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnSnareMediumTomNotes()
+    {
+
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnSnareFloorTomNotes()
+    {
+
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnSnareSplashNotes()
+    {
+
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnSnareRideNotes()
+    {
+
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnSmallTomKickDrumNotes()
+    {
+
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnSmallTomMediumTomNotes()
+    {
+
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnSmallTomFloorTomNotes()
+    {
+
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnSmallTomSplashNotes()
+    {
+
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnSmallTomRideNotes()
+    {
+
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnKickDrumMediumTomNotes()
+    {
+
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnKickDrumFloorTomNotes()
+    {
+
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnKickDrumSplashNotes()
+    {
+
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnKickDrumRideNotes()
+    {
+
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnMediumTomFloorTomNotes()
+    {
+
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnMediumTomSplashNotes()
+    {
+
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnMediumTomRideNotes()
+    {
+
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnFloorTomSplashNotes()
+    {
+
+        SpawnNoteOfType(6);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnFloorTomRideNotes()
+    {
+
+        SpawnNoteOfType(6);
+        SpawnNoteOfType(8);
+
+    }
+    public void SpawnSplashRideNotes()
+    {
+
+        SpawnNoteOfType(7);
+        SpawnNoteOfType(8);
+
+    }
+    #endregion
+
+    #region TripleNotes
     //Triple Notes
+    public void SpawnHiHatCrashSnareNotes()
+    {
 
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(2);
+
+    }
+    public void SpawnHiHatCrashSmallTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnHiHatCrashKickDrumNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnHiHatCrashMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatCrashFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatCrashSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatCrashRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatSnareSmallTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnHiHatSnareKickDrumNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnHiHatSnareMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatSnareFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatSnareSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatSnareRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatSmallTomKickDrumNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(4);
+
+    }
+    public void SpawnHiHatSmallTomMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatSmallTomFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatSmallTomSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatSmallTomRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatKickDrumMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatKickDrumFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatKickDrumSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatKickDrumRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatMediumTomFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatMediumTomSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatMediumTomRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatFloorTomSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(6);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatFloorTomRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(6);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatSplashRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(7);
+        SpawnNoteOfType(8);
+
+    }
+    #endregion
+
+    #region QuadNotes
     //Quad Notes
+    public void SpawnHiHatKickDrumCrashSnareNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(2);
+
+    }
+    public void SpawnHiHatKickDrumCrashSmallTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnHiHatKickDrumCrashMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatKickDrumCrashFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatKickDrumCrashSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatKickDrumCrashRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(1);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatKickDrumSnareSmallTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(3);
+
+    }
+    public void SpawnHiHatKickDrumSnareMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatKickDrumSnareFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatKickDrumSnareSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatKickDrumSnareRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(2);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatKickDrumSmallTomMediumTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(5);
+
+    }
+    public void SpawnHiHatKickDrumSmallTomFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatKickDrumSmallTomSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatKickDrumSmallTomRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(3);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatKickDrumMediumTomFloorTomNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(6);
+
+    }
+    public void SpawnHiHatKickDrumMediumTomSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatKickDrumMediumTomRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(5);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatKickDrumFloorTomSplashNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(6);
+        SpawnNoteOfType(7);
+
+    }
+    public void SpawnHiHatKickDrumFloorTomRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(6);
+        SpawnNoteOfType(8);
+
+    }
+
+    public void SpawnHiHatKickDrumSplashRideNotes()
+    {
+
+        SpawnNoteOfType(0);
+        SpawnNoteOfType(4);
+        SpawnNoteOfType(7);
+        SpawnNoteOfType(8);
+
+    }
+    #endregion
+
+    #endregion Notes End
 
     public void SpawnNoteCombination(int[] noteTypes)
     {
