d5e8a581e93eb299a4e9c19db7d0b68dc380963d
BattlestationX2\JDavies <MilesJMaiden@Gmail.com>
2024-03-03 10:23:27 +0000

    PlayModeManager.CS checked and tidied + Quad notes. IMPORTANT: There are 94 different combinations of notes that can be played with the 9 percussion instruments in tr drum set. (SINGLE NOTES = 9, DOUBLES = 36, 28 TRIPLES, 21 QUADS).

--- BeatBoxVR/Assets/Scripts/PlayMode/PlayModeManager.cs
+++ BeatBoxVR/Assets/Scripts/PlayMode/PlayModeManager.cs
@@ -56,22 +56,6 @@ public class PlayModeManager : MonoBehaviour
         SwitchToSongWithDelay(1); // Index of the second song
     }
 
-    // Method to spawn notes based on rules
-    public void SpawnNotes(int[] noteTypes)
-    {
-        bool includesHiHat = System.Array.IndexOf(noteTypes, 0) != -1; // Check if HiHat is included
-        bool includesKickDrum = System.Array.IndexOf(noteTypes, 4) != -1; // Check if KickDrum is included
-
-        // Calculate max notes allowed
-        int maxNotes = 2 + (includesHiHat ? 1 : 0) + (includesKickDrum ? 1 : 0);
-
-        // Spawn notes up to the max allowed
-        for (int i = 0; i < Mathf.Min(noteTypes.Length, maxNotes); i++)
-        {
-            SpawnNoteOfType(noteTypes[i]);
-        }
-    }
-
     public void SpawnNoteOfType(params int[] noteTypes)
     {
         foreach (int noteType in noteTypes)
@@ -98,498 +82,371 @@ public class PlayModeManager : MonoBehaviour
     #endregion
 
     #region DoubleNotes
-    //DoubleNotes
-    public void SpawnHiHatCrashNotes() { 
-        
+    //DoubleNotes - 36
+    public void SpawnHiHatCrashNotes() 
+    { 
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
-
     }
-    public void SpawnHiHatSnarehNotes()
+    public void SpawnHiHatSnareNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
-
     }
-    public void SpawnHiHatSmallTomhNotes()
+    public void SpawnHiHatSmallTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(3);
-
     }
-    public void SpawnHiHatKickDrumhNotes()
+    public void SpawnHiHatKickDrumNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
-
     }
     public void SpawnHiHatMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatRidehNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnCrashSnareNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(2);
-
     }
     public void SpawnCrashSmallTomNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(3);
-
     }
     public void SpawnCrashKickDrumNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(4);
-
     }
     public void SpawnCrashMediumTomNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(5);
-
     }
     public void SpawnCrashFloorTomNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(6);
-
     }
     public void SpawnCrashSplashNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(7);
-
     }
     public void SpawnCrashRideNotes()
     {
-
         SpawnNoteOfType(1);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnSnareSmallTomNotes()
     {
-
         SpawnNoteOfType(2);
         SpawnNoteOfType(3);
-
     }
     public void SpawnSnareKickDrumTomNotes()
     {
-
         SpawnNoteOfType(2);
         SpawnNoteOfType(4);
-
     }
     public void SpawnSnareMediumTomNotes()
     {
-
         SpawnNoteOfType(2);
         SpawnNoteOfType(5);
-
     }
     public void SpawnSnareFloorTomNotes()
     {
-
         SpawnNoteOfType(2);
         SpawnNoteOfType(6);
-
     }
     public void SpawnSnareSplashNotes()
     {
-
         SpawnNoteOfType(2);
         SpawnNoteOfType(7);
-
     }
     public void SpawnSnareRideNotes()
     {
-
         SpawnNoteOfType(2);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnSmallTomKickDrumNotes()
     {
-
         SpawnNoteOfType(3);
         SpawnNoteOfType(4);
-
     }
     public void SpawnSmallTomMediumTomNotes()
     {
-
         SpawnNoteOfType(3);
         SpawnNoteOfType(5);
-
     }
     public void SpawnSmallTomFloorTomNotes()
     {
-
         SpawnNoteOfType(3);
         SpawnNoteOfType(6);
-
     }
     public void SpawnSmallTomSplashNotes()
     {
-
         SpawnNoteOfType(3);
         SpawnNoteOfType(7);
-
     }
     public void SpawnSmallTomRideNotes()
     {
-
         SpawnNoteOfType(3);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnKickDrumMediumTomNotes()
     {
-
         SpawnNoteOfType(4);
         SpawnNoteOfType(5);
-
     }
     public void SpawnKickDrumFloorTomNotes()
     {
-
         SpawnNoteOfType(4);
         SpawnNoteOfType(6);
-
     }
     public void SpawnKickDrumSplashNotes()
     {
-
         SpawnNoteOfType(4);
         SpawnNoteOfType(7);
-
     }
     public void SpawnKickDrumRideNotes()
     {
-
         SpawnNoteOfType(4);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnMediumTomFloorTomNotes()
     {
-
         SpawnNoteOfType(5);
         SpawnNoteOfType(6);
-
     }
     public void SpawnMediumTomSplashNotes()
     {
-
         SpawnNoteOfType(5);
         SpawnNoteOfType(7);
-
     }
     public void SpawnMediumTomRideNotes()
     {
-
         SpawnNoteOfType(5);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnFloorTomSplashNotes()
     {
-
         SpawnNoteOfType(6);
         SpawnNoteOfType(7);
-
     }
     public void SpawnFloorTomRideNotes()
     {
-
         SpawnNoteOfType(6);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnSplashRideNotes()
     {
-
         SpawnNoteOfType(7);
         SpawnNoteOfType(8);
-
     }
     #endregion
 
     #region TripleNotes
-    //Triple Notes
+    //Triple Notes - 28
     public void SpawnHiHatCrashSnareNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(2);
-
     }
     public void SpawnHiHatCrashSmallTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(3);
-
     }
     public void SpawnHiHatCrashKickDrumNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(4);
-
     }
     public void SpawnHiHatCrashMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatCrashFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatCrashSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatCrashRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(1);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatSnareSmallTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
         SpawnNoteOfType(3);
-
     }
     public void SpawnHiHatSnareKickDrumNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
         SpawnNoteOfType(4);
-
     }
     public void SpawnHiHatSnareMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatSnareFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatSnareSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatSnareRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(2);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatSmallTomKickDrumNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(3);
         SpawnNoteOfType(4);
-
     }
     public void SpawnHiHatSmallTomMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(3);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatSmallTomFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(3);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatSmallTomSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(3);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatSmallTomRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(3);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatKickDrumMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatKickDrumFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatKickDrumSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatKickDrumRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatMediumTomFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(5);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatMediumTomSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(5);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatMediumTomRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(5);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatFloorTomSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(6);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatFloorTomRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(6);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatSplashRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(7);
         SpawnNoteOfType(8);
-
     }
     #endregion
 
@@ -597,71 +454,56 @@ public class PlayModeManager : MonoBehaviour
     //Quad Notes
     public void SpawnHiHatKickDrumCrashSnareNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(1);
         SpawnNoteOfType(2);
-
     }
     public void SpawnHiHatKickDrumCrashSmallTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(1);
         SpawnNoteOfType(3);
-
     }
     public void SpawnHiHatKickDrumCrashMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(1);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatKickDrumCrashFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(1);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatKickDrumCrashSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(1);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatKickDrumCrashRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(1);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatKickDrumSnareSmallTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(2);
         SpawnNoteOfType(3);
-
     }
     public void SpawnHiHatKickDrumSnareMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(2);
@@ -670,132 +512,99 @@ public class PlayModeManager : MonoBehaviour
     }
     public void SpawnHiHatKickDrumSnareFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(2);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatKickDrumSnareSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(2);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatKickDrumSnareRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(2);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatKickDrumSmallTomMediumTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(3);
         SpawnNoteOfType(5);
-
     }
     public void SpawnHiHatKickDrumSmallTomFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(3);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatKickDrumSmallTomSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(3);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatKickDrumSmallTomRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(3);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatKickDrumMediumTomFloorTomNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(5);
         SpawnNoteOfType(6);
-
     }
     public void SpawnHiHatKickDrumMediumTomSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(5);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatKickDrumMediumTomRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(5);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatKickDrumFloorTomSplashNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(6);
         SpawnNoteOfType(7);
-
     }
     public void SpawnHiHatKickDrumFloorTomRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(6);
         SpawnNoteOfType(8);
-
     }
 
     public void SpawnHiHatKickDrumSplashRideNotes()
     {
-
         SpawnNoteOfType(0);
         SpawnNoteOfType(4);
         SpawnNoteOfType(7);
         SpawnNoteOfType(8);
-
     }
     #endregion
-
     #endregion Notes End
-
-    public void SpawnNoteCombination(int[] noteTypes)
-    {
-        SpawnNotes(noteTypes);
-    }
-
 }
