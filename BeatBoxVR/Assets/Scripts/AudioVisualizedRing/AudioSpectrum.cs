f08b483d2bf0d783746df96d82e303996589ee88
Runtomaitorun <yeahmy1022@gmail.com>
2024-04-22 19:06:15 +0100

    Get audio spectrum

---
+++ BeatBoxVR/Assets/Scripts/AudioVisualizedRing/AudioSpectrum.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSpectrum : MonoBehaviour
+{
+    public AudioSource audioSource;
+    [SerializeField]private float[] audioSamples = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Temporarily call here
+        GetSpectrumAudioSource();
+        
+    }
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(audioSamples,0,FFTWindow.Blackman);
+    }
+}
